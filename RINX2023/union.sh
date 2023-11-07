ec8f807e999ab6254f80814d534ce556f5998983
mukul1em <mukulrt1997@gmail.com>
2023-11-07 13:35:57 -0500

    rename and update Union

--- RINX2023/union.sh
+++ RINX2023/union.sh
@@ -3,13 +3,22 @@
 USERNAME="postgres"
 DATABASE="postgres"
 
-
-
-
-#!/bin/bash
 echo "Script started at $(date)"
 for i in "/pgdata/rinx/scripts/union/"*
 do
+    # Extract the table name from the file name
+    table_name=$(basename "$i" .sql)
+
+    # Check if the table exists before attempting to drop it
+    if psql -U "$USERNAME" -d "$DATABASE" -c "SELECT 1 FROM $table_name LIMIT 1" 2>/dev/null; then
+        # Table exists, so drop it
+        psql -U "$USERNAME" -d "$DATABASE" -c "DROP TABLE $table_name"
+        echo "Table $table_name dropped."
+    else
+        echo "Table $table_name does not exist. Skipping drop operation."
+    fi
+
+    # Execute the SQL script
     psql -U "$USERNAME" -d "$DATABASE" -f "$i"
 done
 echo "Script finished at $(date)"
