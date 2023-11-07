6b8c2233e4d68d0bf33d305e4aa5bb94a43a13cf
mukul1em <mukulrt1997@gmail.com>
2023-11-07 10:58:02 -0500

    add concat climate results

---
+++ RINX2023/concat_climate_results.py
@@ -0,0 +1,93 @@
+import os
+import pandas as pd
+
+
+def combine_csv_files(path, save_result_to_csv=False, output_path='combined_result.csv'):
+    """
+    Combine multiple CSV files in a directory and merge them into a single DataFrame.
+
+    Args:
+        path (str): Path to the directory containing CSV files.
+        save_result_to_csv (bool, optional): Whether to save the result to a CSV file (default is False).
+        output_path (str, optional): Path to save the combined result (default is 'combined_result.csv').
+
+    Returns:
+        pd.DataFrame: Combined DataFrame.
+
+    Example:
+    combine_csv_files('data_directory', save_result_to_csv=True)
+    """
+    try:
+        combined_data = pd.DataFrame()
+        
+        for file in os.listdir(path):
+            if file.endswith('.csv'):
+                data_path = os.path.join(path, file)
+                data = pd.read_csv(data_path, delimiter='\t', header=None)
+
+                # Extract the variable_name from the file name
+                variable_name = os.path.splitext(file)[0].split("_")[1]
+
+                # Rename columns
+                data.columns = ['grid_id', 'Start_date', variable_name]
+
+                # Merge data based on 'Start_date' and 'grid_id'
+                if combined_data.empty:
+                    combined_data = data
+                else:
+                    combined_data = pd.merge(combined_data, data, on=['Start_date', 'grid_id'], how='outer')
+
+        combined_data['Start_date'] = pd.to_datetime(combined_data['Start_date'], format='%Y%m%d')
+        if save_result_to_csv:
+            combined_data.to_csv(output_path, index=False)
+
+        return combined_data
+    except (FileNotFoundError, pd.errors.EmptyDataError, pd.errors.ParserError, Exception) as e:
+        raise Exception(f"Error combining CSV files: {e}")
+
+
+
+
+def merge_with_climate_data(combined_data, climate_data_path, save_result_to_csv=True, output_path='combined_climate_result.csv'):
+    """
+    Merge climate data with combined data by grid_id and Start_date.
+
+    Args:
+        combined_data (pd.DataFrame): DataFrame containing combined data.
+        climate_data_path (str): Path to the climate data CSV file.
+        save_result_to_csv (bool, optional): Whether to save the result to a CSV file (default is True).
+        output_path (str, optional): Path to save the merged result (default is 'combined_climate_result.csv').
+
+    Returns:
+        pd.DataFrame: Merged DataFrame with climate data.
+
+    Example:
+    merge_with_climate_data(combined_data, 'climate_data.csv', save_result_to_csv=True)
+    """
+    try:
+        climate_data = pd.read_csv(climate_data_path)
+        climate_data['Start_date'] = pd.to_datetime(climate_data['Start_date'], format='%Y%m%d')
+        climate_data['End_date'] = pd.to_datetime(climate_data['End_date'], format='%Y%m%d')
+
+    except (FileNotFoundError, pd.errors.EmptyDataError, pd.errors.ParserError) as e:
+        raise Exception(f"Error reading climate data: {e}")
+
+    try:
+        result = pd.merge(climate_data, combined_data, on=['grid_id', 'Start_date'])
+    except KeyError:
+        raise Exception("Columns 'grid_id' and 'Start_date' not found in combined_data")
+
+    if save_result_to_csv:
+        try:
+            result.to_csv(output_path, index=False)
+        except Exception as e:
+            raise Exception(f"Error saving merged data to CSV: {e}")
+
+    return result
+
+
+if __name__ == "__main__":
+    path = './results'
+    climate_data_path = './climate_input_01.csv'
+    combined_data  = combine_csv_files(path)
+    climate_data_merge = merge_with_climate_data(combined_data, climate_data_path)
