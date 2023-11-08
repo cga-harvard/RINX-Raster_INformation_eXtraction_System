e30b43ca9650df4223aaafebf22cadad02a145d9
dkakkar <devikakakkar29@gmail.com>
2023-11-08 09:09:27 -0500

    Update query_tdmean.sql

--- RINX2023/query/query_tdmean.sql
+++ RINX2023/query/query_tdmean.sql
@@ -5,4 +5,4 @@ inner join tdmeanunion b
 ON
 ST_Intersects(ST_Envelope(b.rast), ST_Transform(ST_SetSRID( ST_Point(a.longitude, a.latitude), 4326), 4269)) = 't'
 AND startdate <= filedate AND enddate >= filedate);
-Copy(Select * from results_tdmean) TO '/pgdata/results/results_tdmean.csv';
+Copy(Select * from results_tdmean) TO '/pgdata/rinx/results/results_tdmean.csv';
