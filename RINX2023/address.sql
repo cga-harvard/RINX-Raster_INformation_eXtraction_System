75c2065284964c0e11aaa0e08c38a938337efd24
mukul1em <mukulrt1997@gmail.com>
2023-11-14 15:58:39 -0500

    add address

---
+++ RINX2023/address.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS addresses;
+Create table addresses (Id character varying(255), latitude float, longitude float, start_date character varying(255), end_date character varying(255));
+copy addresses from '/pgdata/rinx/input/climate_input_02.csv' (FORMAT csv, HEADER, DELIMITER ',');
+Alter table addresses add column startdate date;
+Alter table addresses add column enddate date;
+Update addresses set startdate=TO_DATE(start_date,'YYYYMMDD');
+Update addresses set enddate=TO_DATE(end_date,'YYYYMMDD');
+Alter table addresses drop column start_date;
+Alter table addresses drop column end_date;
+ALTER TABLE addresses ADD COLUMN geom geometry (Point, 4326);
+UPDATE addresses SET geom = ST_SetSRID (ST_MakePoint ( longitude, latitude), 4326);
+create index addresses_id ON addresses USING GIST (geom);
+Cluster addresses using addresses_idx;
